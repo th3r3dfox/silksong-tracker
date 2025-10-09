e81f342303bce02107ddbc1ea737f9f1a556d95b
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-10-09 12:22:09 -0400

    convert

--- js/constants.js
+++ js/constants.js
@@ -2,5 +2,5 @@ export const CSHARP_HEADER = new Uint8Array([
   0, 1, 0, 0, 0, 255, 255, 255, 255, 1, 0, 0, 0, 0, 0, 0, 0, 6, 1, 0, 0, 0,
 ]);
 
-// 🔑 Chiave AES usata da Silksong per cifrare i save
+// 🔑 AES key used by Silksong to encrypt saves
 export const AES_KEY_STRING = "UKu52ePUBwetZ9wNX88o54dnfKRu0T1l";
