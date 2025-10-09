61d11c5e30d3aa3476102505accede471e8d96ca
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-10-08 23:45:45 -0400

    update

---
+++ prettier.config.mjs
@@ -0,0 +1,29 @@
+// This is the configuration file for Prettier, the auto-formatter:
+// https://prettier.io/docs/en/configuration.html
+
+// @ts-check
+
+/** @type {import("prettier").Config} */
+const config = {
+  plugins: [
+    "prettier-plugin-organize-imports", // Prettier does not format imports by default.
+    "prettier-plugin-packagejson", // Prettier does not format "package.json" by default.
+  ],
+
+  overrides: [
+    // Allow proper formatting of JSONC files that have JSON file extensions.
+    {
+      files: ["**/.vscode/*.json"],
+      options: {
+        parser: "jsonc",
+      },
+    },
+  ],
+
+  // We break from the default Prettier config for only a single option: operator position. There
+  // are no known arguments for placing operators at the end of the line, as outlined in this
+  // thread: https://github.com/prettier/prettier/issues/3806
+  experimentalOperatorPosition: "start",
+};
+
+export default config;
