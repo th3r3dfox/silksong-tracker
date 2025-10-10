caebd98dbe59b6f57b91352a6679f1e46410d7d7
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-10-10 13:47:00 -0400

    update

--- scripts/lint.ts
+++ scripts/lint.ts
@@ -5,7 +5,7 @@ await lintCommands(import.meta.dirname, [
   "tsc --noEmit",
   "tsc --noEmit --project ./scripts/tsconfig.json",
 
-  // Use ESLint to lint the TypeScript code.
+  // Use ESLint to lint the code.
   // - "--max-warnings 0" makes warnings fail, since we set all ESLint errors to warnings.
   "eslint --max-warnings 0 .",
 
@@ -13,21 +13,6 @@ await lintCommands(import.meta.dirname, [
   // - "--log-level=warn" makes it only output errors.
   "prettier --log-level=warn --check .",
 
-  // Use Knip to check for unused files, exports, and dependencies.
-  // - "--no-progress" - Don't show dynamic progress updates. Progress is automatically disabled in
-  //   CI environments.
-  // - "--treat-config-hints-as-errors" - Exit with non-zero code (1) if there are any configuration
-  //   hints.
-  "knip --no-progress --treat-config-hints-as-errors",
-
-  // Use CSpell to spell check every file.
-  // - "--no-progress" and "--no-summary" make it only output errors.
-  "cspell --no-progress --no-summary",
-
-  // Check for unused words in the CSpell configuration file.
-  "cspell-check-unused-words",
-
-  // Check for template updates.
-  // @template-ignore-next-line
-  "complete-cli check --ignore LICENSE",
+  "ajv validate -c ajv-formats -d ./data/bosses.json -s ./data/bosses.schema.json",
+  "ajv validate -c ajv-formats -d ./data/wishes.json -s ./data/wishes.schema.json",
 ]);
