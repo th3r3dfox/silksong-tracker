14d39dbe72b654a03b659ccd27ac2a525655fa7c
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-10-12 15:27:26 -0400

    update

---
+++ eslint.config.json.mjs
@@ -0,0 +1,26 @@
+// This is the configuration file for ESLint, the TypeScript linter:
+// https://eslint.org/docs/latest/use/configure/
+
+// We need to make a separate configuration file for JSON files because JavaScript/TypeScript rules
+// will throw errors when applied to JSON files.
+
+// @ts-check
+
+import ESLintPluginJSON from "@eslint/json";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig({
+  plugins: {
+    json: ESLintPluginJSON,
+  },
+  files: ["src/data/*.json"],
+  language: "json/json",
+  rules: {
+    "json/no-duplicate-keys": "error",
+    "json/no-empty-keys": "error",
+    "json/no-unnormalized-keys": "error",
+    "json/no-unsafe-values": "error",
+    "json/sort-keys": "error",
+    "json/top-level-interop": "error",
+  },
+});
