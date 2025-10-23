756ccd0acacc091d30eddf385c98ebb657c9670a
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-10-23 08:26:46 -0400

    update

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -17,35 +17,64 @@ export default {
 
     // Duplicate
     "declaration-block-no-duplicate-custom-properties": true,
-    /// "declaration-block-no-duplicate-properties": true,
-    /// "font-family-no-duplicate-names": true,
-    /// "keyframe-block-no-duplicate-selectors": true,
-    /// "no-duplicate-at-import-rules": true,
+    "declaration-block-no-duplicate-properties": true,
+    "font-family-no-duplicate-names": true,
+    "keyframe-block-no-duplicate-selectors": true,
+    "no-duplicate-at-import-rules": true,
     "no-duplicate-selectors": true,
 
     // Empty
-    // TODO
+    "block-no-empty": true,
+    "comment-no-empty": true,
+    "no-empty-source": true,
 
     // Invalid
-    // TODO
+    "at-rule-prelude-no-invalid": true,
+    "color-no-invalid-hex": true,
+    "function-calc-no-unspaced-operator": true,
+    "keyframe-declaration-no-important": true,
+    "media-query-no-invalid": true,
+    "named-grid-areas-no-invalid": true,
+    "no-invalid-double-slash-comments": true,
+    "no-invalid-position-at-import-rule": true,
+    "no-invalid-position-declaration": true,
+    "string-no-newline": true,
+    "syntax-string-no-invalid": true,
 
     // Irregular
-    // TODO
+    "no-irregular-whitespace": true,
 
     // Missing
-    // TODO
+    "custom-property-no-missing-var-function": true,
+    "font-family-no-missing-generic-family-keyword": true,
+    "nesting-selector-no-missing-scoping-root": true,
 
     // Non-standard
-    // TODO
+    "function-linear-gradient-no-nonstandard-direction": true,
 
     // Overrides
-    // TODO
+    "declaration-block-no-shorthand-property-overrides": true,
 
     // Unmatchable
-    // TODO
+    "selector-anb-no-unmatchable": true,
 
     // Unknown
-    // TODO
+    "annotation-no-unknown": true,
+    "at-rule-descriptor-no-unknown": true,
+    "at-rule-descriptor-value-no-unknown": true,
+    "at-rule-no-unknown": true,
+    "declaration-property-value-no-unknown": true,
+    "function-no-unknown": true,
+    "media-feature-name-no-unknown": true,
+    "media-feature-name-value-no-unknown": true,
+    "no-unknown-animations": true,
+    "no-unknown-custom-media": true,
+    "no-unknown-custom-properties": true,
+    "property-no-unknown": true,
+    "selector-pseudo-class-no-unknown": true,
+    "selector-pseudo-element-no-unknown": true,
+    "selector-type-no-unknown": true,
+    "unit-no-unknown": true,
 
     // -------------------
     // Enforce conventions
